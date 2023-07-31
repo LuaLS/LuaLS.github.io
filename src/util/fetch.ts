dacee521ed1384953a47be37c7654b163eb7c588
carsakiller <carsakiller@gmail.com>
2023-07-31 19:38:43 -0400

    doc: add fetch cache location for windows

--- src/util/fetch.ts
+++ src/util/fetch.ts
@@ -1,6 +1,10 @@
 import defaultFetch, { fetchBuilder, FileSystemCache } from "node-fetch-cache";
 import os from "os";
 
+/* Cache location:
+ Windows: C:\Users\USER\AppData\Local\Temp\LuaLS_website_cache
+*/
+
 const cacheDir = os.tmpdir() + "/LuaLS_website_cache/";
 const cacheDuration = 1800000; // 30 minutes;
 const cacheFetch = fetchBuilder.withCache(
