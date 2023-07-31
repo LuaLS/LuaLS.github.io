ed241aaf73fc1660301b0d53a4f1acbe897d056d
carsakiller <carsakiller@gmail.com>
2023-07-31 16:53:29 -0400

    add: fetch that caches responses for development
    Helps prevent vite from DDOSing APIs

---
+++ src/helpers/fetch.ts
@@ -0,0 +1,24 @@
+import defaultFetch, { fetchBuilder, FileSystemCache } from "node-fetch-cache";
+import os from "os";
+
+const cacheDir = os.tmpdir() + "/LuaLS_website_cache/";
+const cacheDuration = 1800000; // 30 minutes;
+const cacheFetch = fetchBuilder.withCache(
+  new FileSystemCache({ cacheDirectory: cacheDir, ttl: cacheDuration })
+);
+
+/** If in development mode, caches requests to prevent vite from DDOSing api endpoints */
+export default async (...args: Parameters<typeof defaultFetch>) => {
+  if (import.meta.env.PROD) {
+    return await fetch(...(args as [RequestInfo, RequestInit]));
+  } else {
+    return await cacheFetch(...args).then(async (response) => {
+      if (!response.ok) {
+        await response.ejectFromCache();
+        throw new Error(`${response.status}: ${response.statusText}`);
+      } else {
+        return response;
+      }
+    });
+  }
+};
