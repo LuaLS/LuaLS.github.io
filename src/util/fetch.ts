0850c9b22049b1e945886141dde4643f886c53b9
carsakiller <carsakiller@gmail.com>
2024-04-05 02:50:57 +0000

    chore: update node-fetch-cache

--- src/util/fetch.ts
+++ src/util/fetch.ts
@@ -1,23 +1,27 @@
-import {fetchBuilder, FileSystemCache } from "node-fetch-cache";
+import NodeFetchCache, { FileSystemCache } from "node-fetch-cache";
 import type { default as nodeFetch } from "node-fetch";
 import os from "os";
 
 /* Cache location:
  Windows: C:\Users\USER\AppData\Local\Temp\LuaLS_website_cache
+ Linux: /tmp/LuaLS_website_cache
 */
 
 const cacheDir = os.tmpdir() + "/LuaLS_website_cache/";
 const cacheDuration = 1800000; // 30 minutes;
-const cacheFetch = fetchBuilder.withCache(
-  new FileSystemCache({ cacheDirectory: cacheDir, ttl: cacheDuration })
-);
+const cacheFetch = NodeFetchCache.create({
+  cache: new FileSystemCache({ cacheDirectory: cacheDir, ttl: cacheDuration }),
+});
 
 /** If in development mode, caches requests to prevent vite from DDOSing api endpoints */
 export default async (request: RequestInfo, init?: RequestInit) => {
   if (import.meta.env.PROD) {
     return await fetch(request, init);
   } else {
-    return await cacheFetch(request as Parameters<typeof nodeFetch>[0], init as Parameters<typeof nodeFetch>[1]).then(async (response) => {
+    return await cacheFetch(
+      request as Parameters<typeof nodeFetch>[0],
+      init as Parameters<typeof nodeFetch>[1]
+    ).then(async (response) => {
       if (!response.ok) {
         await response.ejectFromCache();
         throw new Error(`${response.status}: ${response.statusText}`);
