a764e48609fff1c4a1cf92019ecc048d77d3ffad
carsakiller <carsakiller@gmail.com>
2024-06-27 19:33:06 +0000

    fix: extension installs retrieval
    Install count is now retrieved at build time due to CORS

---
+++ src/util/vscode.ts
@@ -0,0 +1,24 @@
+import path from "path";
+import util from "util";
+
+const exec = util.promisify((await import("child_process")).exec);
+
+const pathToVSCE = path.resolve("node_modules/.bin/vsce");
+
+export const getExtensionInstalls = async (
+  extensionName: string
+): Promise<number> => {
+  try {
+    const { stdout, stderr } = await exec(
+      `${pathToVSCE} show ${extensionName}`
+    );
+    const match = /install\s*(\d+)\s*installs/g.exec(stdout);
+    if (match === null) {
+      throw new Error("Failed to parse out install count");
+    }
+    console.log(match)
+    return Number(match[1]);
+  } catch (e) {
+    throw new Error("Failed to get extension information", { cause: e });
+  }
+};
